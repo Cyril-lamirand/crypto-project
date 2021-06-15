2cb7d02ee94e2f302d91daa0ddac9aa6c89a7f19
Cyril-lamirand <contact.cyril1@gmail.com>
2021-06-16 00:54:17 +0200

    [Init] Initialization Project
    - Adding AXIOS
    - Adding Bootstrap-vue
    - Adding FontAwesome

---
+++ src/components/Etherum.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="etherum-component">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Etherum"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
+import './plugins/axios'
 import App from './App.vue'
+
+// Bootstrap-Vue
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+// FontAwesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faUserSecret)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
