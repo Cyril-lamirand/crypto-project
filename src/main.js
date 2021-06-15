30733af5c9f409bf79f3e0a5586ee3496c54c93b
Cyril-lamirand <contact.cyril1@gmail.com>
2021-06-16 00:56:47 +0200

    [Add] Dependency
    - Add Vue-router

--- src/main.js
+++ src/main.js
@@ -14,11 +14,13 @@ Vue.use(IconsPlugin)
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import router from './router'
 library.add(faUserSecret)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
